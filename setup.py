bf9a63a0551b949ec943394de8cdf34d5b51e6a7
dotpot <halfas.online@gmail.com>
2017-06-14 14:36:55 +0300

    excluding tests from the build

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='pushit',
     version='0.1',
-    packages=['pushit'],
+    packages=find_packages(exclude=("tests",)),
     install_requires=['pytest', 'pytest-mock', 'ujson'],
     description='Push notifications sender for ios and android',
     keywords='push notifications apple ios android google cloud messaging',
